5b9f2ea34cc638d811641f895927fa173a9c70b6
Phan Long <phanmainhatlong@outlook.com>
2025-12-02 10:41:05 +0700

    Scratch space & SLN Structure

---
+++ ImageAutomate/Scratch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+using Image image = new Image<Rgba32>(100, 100);
+
+Star star = new(50, 50, 5, 20, 45);
+
+PointF[] points = star.Points.ToArray();
+
+Color[] colors =
+{
+    Color.Red
+};
+
+PathGradientBrush brush = new(points, colors);
+
+image.Mutate(x => x.Fill(brush).Rotate(180));
+
+image.Save("star.png");
