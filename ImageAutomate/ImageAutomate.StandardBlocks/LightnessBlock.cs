02f1f36c6d9a35a21c3483d5a6fbb5dbd8f574bc
tridominh-cloud <minhtri2416@gmail.com>
2025-12-18 01:35:10 +0700

    add Lightness Block

---
+++ ImageAutomate/ImageAutomate.StandardBlocks/LightnessBlock.cs
@@ -0,0 +1,153 @@
+﻿using ImageAutomate.Core;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAutomate.StandardBlocks;
+
+public class LightnessBlock : IBlock
+{
+    #region Fields
+
+    private readonly IReadOnlyList<Socket> _inputs = [new("Lightness.In", "Image.In")];
+    private readonly IReadOnlyList<Socket> _outputs = [new("Lightness.Out", "Image.Out")];
+
+    private bool _disposed;
+
+    private int _nodeWidth = 200;
+    private int _nodeHeight = 100;
+
+    private float _lightAmount;
+    #endregion
+
+    #region IBlock basic
+
+    public string Name => "Lightness";
+
+    public string Title => "Lightness";
+
+    public string Content => $"Light amount: {LighAmount}";
+
+    #endregion
+
+    #region Layout
+
+    [Category("Layout")]
+    [Description("Width of the block node")]
+    public int Width
+    {
+        get => _nodeWidth;
+        set
+        {
+            if (_nodeWidth != value)
+            {
+                _nodeWidth = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+    }
+
+    [Category("Layout")]
+    [Description("Height of the block node")]
+    public int Height
+    {
+        get => _nodeHeight;
+        set
+        {
+            if (_nodeHeight != value)
+            {
+                _nodeHeight = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+    }
+
+    #endregion
+
+    #region Sockets
+
+    public IReadOnlyList<Socket> Inputs => _inputs;
+    public IReadOnlyList<Socket> Outputs => _outputs;
+
+    #endregion
+
+    #region Configuration
+
+    [Category("Configuration")]
+    [Description("0 will create a black image. A value of 1 leaves the input unchanged")]
+    public float LighAmount
+    {
+        get => _lightAmount;
+        set
+        {
+            if (_lightAmount != value)
+            {
+                _lightAmount = value;
+                OnPropertyChanged(nameof(_lightAmount));
+            }
+        }
+    }
+
+    #endregion
+
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    #endregion
+
+    #region Execute
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<Socket, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        return Execute(inputs.ToDictionary(kvp => kvp.Key.Id, kvp => kvp.Value));
+    }
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<string, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        if (!inputs.TryGetValue(_inputs[0].Id, out var inItems))
+            throw new ArgumentException($"Input items not found for the expected input socket {_inputs[0].Id}.", nameof(inputs));
+
+        var outputItems = new List<IBasicWorkItem>();
+
+        foreach (var sourceItem in inItems.OfType<WorkItem>())
+        {
+            sourceItem.Image.Mutate(x => x.Lightness(LighAmount));
+            outputItems.Add(sourceItem);
+        }
+
+        return new Dictionary<Socket, IReadOnlyList<IBasicWorkItem>>
+            {
+                { _outputs[0], outputItems }
+            };
+    }
+
+    #endregion
+
+    #region IDisposable
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+}
