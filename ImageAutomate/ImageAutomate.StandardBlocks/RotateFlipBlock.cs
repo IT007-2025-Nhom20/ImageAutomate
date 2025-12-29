8320b85bb33d1507bf862f67401afafab3d3797b
tridominh-cloud <minhtri2416@gmail.com>
2025-12-30 00:04:02 +0700

    add rotateflip

---
+++ ImageAutomate/ImageAutomate.StandardBlocks/RotateFlipBlock.cs
@@ -0,0 +1,248 @@
+﻿using ImageAutomate.Core;
+using SixLabors.ImageSharp.Processing;
+using System.ComponentModel;
+
+namespace ImageAutomate.StandardBlocks;
+
+public class RotateFlipBlock : IBlock
+{
+    #region Fields
+
+    private readonly IReadOnlyList<Socket> _inputs = [new("RotateFlip.In", "Image.In")];
+    private readonly IReadOnlyList<Socket> _outputs = [new("RotateFlip.Out", "Image.Out")];
+
+    private bool _disposed;
+
+    // Configuration fields
+    private RotateMode _rotateMode = RotateMode.None;
+    private FlipMode _flipMode = FlipMode.None;
+
+    // Layout fields
+    private double _x;
+    private double _y;
+    private int _width;
+    private int _height;
+    private string _title = "Rotate & Flip";
+
+    #endregion
+
+    public RotateFlipBlock()
+        : this(200, 100) 
+    {
+    }
+
+    public RotateFlipBlock(int width, int height)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    #region IBlock basic
+
+    [Browsable(false)]
+    public string Name => "RotateFlip";
+
+    [Category("Title")]
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (_title != value)
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+
+    [Browsable(false)]
+    public string Content => $"{RotateMode}, {FlipMode}";
+
+    #endregion
+
+    #region Layout Properties
+
+    /// <inheritdoc />
+    [Category("Layout")]
+    public double X
+    {
+        get => _x;
+        set
+        {
+            if (Math.Abs(_x - value) > double.Epsilon)
+            {
+                _x = value;
+                OnPropertyChanged(nameof(X));
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    [Category("Layout")]
+    public double Y
+    {
+        get => _y;
+        set
+        {
+            if (Math.Abs(_y - value) > double.Epsilon)
+            {
+                _y = value;
+                OnPropertyChanged(nameof(Y));
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    [Category("Layout")]
+    public int Width
+    {
+        get => _width;
+        set
+        {
+            if (_width != value)
+            {
+                _width = value;
+                OnPropertyChanged(nameof(Width));
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    [Category("Layout")]
+    public int Height
+    {
+        get => _height;
+        set
+        {
+            if (_height != value)
+            {
+                _height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+    }
+
+    #endregion
+
+    #region Sockets
+
+    [Browsable(false)]
+    public IReadOnlyList<Socket> Inputs => _inputs;
+    [Browsable(false)]
+    public IReadOnlyList<Socket> Outputs => _outputs;
+
+    #endregion
+
+    #region Configuration
+
+    [Category("Configuration")]
+    [Description("Rotation step (None, 90, 180, 270). Applied before flipping.")]
+    public RotateMode RotateMode
+    {
+        get => _rotateMode;
+        set
+        {
+            if (_rotateMode != value)
+            {
+                _rotateMode = value;
+                OnPropertyChanged(nameof(RotateMode));
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+    }
+
+    [Category("Configuration")]
+    [Description("Flip direction (None, Horizontal, Vertical). Applied after rotation.")]
+    public FlipMode FlipMode
+    {
+        get => _flipMode;
+        set
+        {
+            if (_flipMode != value)
+            {
+                _flipMode = value;
+                OnPropertyChanged(nameof(FlipMode));
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+    }
+
+    #endregion
+
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    #endregion
+
+    #region Execute
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<Socket, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        return Execute(inputs, CancellationToken.None);
+    }
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<Socket, IReadOnlyList<IBasicWorkItem>> inputs, CancellationToken cancellationToken)
+    {
+        return Execute(inputs.ToDictionary(kvp => kvp.Key.Id, kvp => kvp.Value), cancellationToken);
+    }
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<string, IReadOnlyList<IBasicWorkItem>> inputs)
+    {
+        return Execute(inputs, CancellationToken.None);
+    }
+
+    public IReadOnlyDictionary<Socket, IReadOnlyList<IBasicWorkItem>> Execute(
+        IDictionary<string, IReadOnlyList<IBasicWorkItem>> inputs, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(inputs, nameof(inputs));
+        if (!inputs.TryGetValue(_inputs[0].Id, out var inItems))
+            throw new ArgumentException($"Input items not found for the expected input socket {_inputs[0].Id}.", nameof(inputs));
+
+        var outputItems = new List<IBasicWorkItem>();
+
+        foreach (var sourceItem in inItems.OfType<WorkItem>())
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (RotateMode != RotateMode.None || FlipMode != FlipMode.None)
+            {
+                sourceItem.Image.Mutate(x => x.RotateFlip(RotateMode, FlipMode));
+            }
+
+            outputItems.Add(sourceItem);
+        }
+
+        return new Dictionary<Socket, IReadOnlyList<IBasicWorkItem>>
+            {
+                { _outputs[0], outputItems }
+            };
+    }
+
+    #endregion
+
+    #region IDisposable
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
+}
